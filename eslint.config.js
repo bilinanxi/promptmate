fc9f97e7f2227d0fb43f6e8d522350ebfb3d487e
bilinanxi <198214202+bilinanxi@users.noreply.github.com>
2026-07-14 19:02:21 +0800

    chore(tauri): add Windows desktop shell

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,14 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'prompt-assistant-prototype.html'] },
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'src-tauri/target/**',
+      'prompt-assistant-prototype.html',
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
